505cac8b668fadc31846ec18100446323d39ab95
ephmo <241746745+ephmo@users.noreply.github.com>
2025-12-27 14:08:29 +0200

    feat: add log logic

--- src/bin/reprofed-apply.sh
+++ src/bin/reprofed-apply.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 set -euo pipefail
-shopt -s nullglob
+shopt -s globstar nullglob
+
+SCRIPT_PATH="$(readlink -f "${BASH_SOURCE[0]}")"
+SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
+
+source "$SCRIPT_DIR"/../core/constants.sh
+
+LOG_FILE="/var/log/$APP_NAME.log"
+
+: > "$LOG_FILE"
+
+msg_success() {
+  echo "$(date '+%Y-%m-%d %H:%M:%S') SUCCESS: $1" >> "$LOG_FILE"
+}
+
+msg_error() {
+  echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR: $1" >> "$LOG_FILE"
+}
