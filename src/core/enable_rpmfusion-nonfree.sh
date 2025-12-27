cf661cfde1a9e8282c2681a9c9dbee97279cc635
ephmo <241746745+ephmo@users.noreply.github.com>
2025-12-27 22:12:15 +0200

    feat: add enable rpmfusion-nonfree logic

--- src/core/enable_rpmfusion-nonfree.sh
+++ src/core/enable_rpmfusion-nonfree.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 shopt -s globstar nullglob
+
+REPO_ID="rpmfusion-nonfree"
+
+dnf5 config-manager setopt fedora-cisco-openh264.enabled=1
+
+if ! dnf5 repo list --enabled | grep -q "^${REPO_ID}"; then
+  dnf5 install -y https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+fi
