9636731d05345c955a39740f4106a199b9672173
ephmo <241746745+ephmo@users.noreply.github.com>
2025-12-30 23:04:10 +0200

    refactor: restructure project and add package installation logic

--- src/core/check_root.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-shopt -s globstar nullglob
-
-if [ "$EUID" -ne 0 ]; then
-  echo "This script must be run as root or with sudo."
-  exit 1
-fi
--- src/core/constants.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-shopt -s globstar nullglob
-
-APP_NAME="reprofed"
-APP_PATH="/opt/$APP_NAME"
